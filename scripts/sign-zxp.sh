fd8dd97cee7cfcb0e3745ea447af9170697e4cc9
Rory Duncan <robust.rory@gmail.com>
2019-06-02 19:44:12 -0700

    Add cross OS signing

---
+++ scripts/sign-zxp.sh
@@ -0,0 +1,26 @@
+# This script uses the self-signed script to bundle and sign our extension into a zxp file
+#
+#
+# Signature
+#
+#     ZXPSignCmd -sign <inputDir> <outputZxp> <p12> <p12Password> [options]
+# 
+# http://wwwimages.adobe.com/www.adobe.com/content/dam/acom/en/devnet/creativesuite/pdfs/SigningTechNote_CC.pdf
+
+inputdir="build"
+outputdir="dist/fuse-fonts.zxp"
+
+p12="certificate.p12"
+p12password="bravecoffee"
+
+timestampserver="http://sha1timestamp.ws.symantec.com/sha1/timestamp"
+
+echo "Signing and packaging '$inputdir' -> '$outputdir'"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  bin/ZXPSignCmd.dmg -sign "$inputdir" "$output" "$p12" "$p12password" -tsa "$timestampserver"
+else
+  bin/ZXPSignCmd.exe -sign "$inputdir" "$output" "$p12" "$p12password" -tsa "$timestampserver"
+fi
+
+exit 0
