b280cdee64bd88c93883daec1a830998a982a246
Rory Duncan <robust.rory@gmail.com>
2019-06-02 19:58:02 -0700

    Add zxp signature verification script

---
+++ scripts/verify.sh
@@ -0,0 +1,21 @@
+#
+# This script verifies that the .zxp created with sign.zxp.sh is valid.
+#
+
+# Signature
+#
+#     ZXPSignCmd -verify <zxp>|<extensionRootDir> [options]
+#
+# http://wwwimages.adobe.com/www.adobe.com/content/dam/acom/en/devnet/creativesuite/pdfs/SigningTechNote_CC.pdf
+
+zxppath="dist/fuse-fonts.zxp"
+
+echo -e "\nVerifying '$zxppath'..."
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  bin/ZXPSignCmd.dmg -verify "$zxppath"
+else
+  bin/ZXPSignCmd.exe -verify "$zxppath"
+fi
+
+exit 0
