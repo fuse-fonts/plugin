731784ba61790b788303edd808f78fe05bb2995e
Rory Duncan <robust.rory@gmail.com>
2019-06-23 00:08:13 -0700

    Add loading functionality

--- src/stores/typefaces.js
+++ src/stores/typefaces.js
@@ -13,10 +13,14 @@ export const typefaces = writable({});
 export const loading = writable(true);
 
 //
+console.log("loading")
 typefaceService.load().then( data => {
   typefaces.set(data);
   loading.set(false);
-  
+  // window.setTimeout(() => {
+  //   console.log("loaded")
+  //   loading.set(false);
+  // }, 5000);
   // after the real data is laoded — any changes we then save to local storage
   typefaces.subscribe(data => typefaceService.save(data));
 });
