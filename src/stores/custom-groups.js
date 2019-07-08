e330c91b06529e50d6fba8683386ff15921f6437
Rory Duncan <robust.rory@gmail.com>
2019-07-07 19:13:25 -0700

    Add rendering of font faces

--- src/stores/custom-groups.js
+++ src/stores/custom-groups.js
@@ -27,6 +27,7 @@ export const loadCustomGroups = (typefaces) => {
   // create our "all fonts" group using all the typfaces
   allFonts.updateTypeFaces(typefaces);
   allFontsGroup.set(allFonts)
+  selected.set(allFonts);
 
   return customGroupRepository.load(typefaces).then((data) => {
     if (data !== null) {
