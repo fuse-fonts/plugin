45e2efb064bf0e97b858f3db00f58d668e292af4
Rory Duncan <robust.rory@gmail.com>
2019-07-23 23:34:25 -0700

    Add theme detect / watching

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 
+import { detectTheme, addListener } from "helpers/theme.js";
+
+detectTheme();
+addListener();
+
 import App from "App.html";
 
 const appLoader = new App({
   target: document.querySelector("main#app"),
 });
+
