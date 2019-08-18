18de3366a8caebcc6aa993b304e1af7ed3ad14a9
Rory Duncan <robust.rory@gmail.com>
2019-08-18 15:37:38 -0700

    Add window.cep environment polyfill

--- public/scripts/adobe-interface.js
+++ public/scripts/adobe-interface.js
@@ -1,17 +1,19 @@
 // this file let's us use adobe's CS-interface without having to worry
 
-
+class NotImplemented extends Error {
+  constructor(methodname, ...params) {
+    super(...params);
+    this.message = `${methodname} is not implemented.`
+    this.fileName = "adobe-interface.js";
+  }
+}
 /**
  * the host environment we will use internally.
  * this is obtained by calling the following line in the console of an actual running plugin
  * 
  *    JSON.stringify(new CSInterface().hostEnvironment, null, 2)
  * 
- //
- // 
- // 
  */
-// 
 const _hostEnvironment = {
 
   appVersion: "20.0.6",
@@ -173,4 +175,48 @@ if (!window.__adobe_cep__) {
       console.log("__adobe_cep__.showAAM called")
     },
   };
+
+}
+
+// the main functions of window.cep are within cep.util and cep.fs
+// cep.process and cep.encoding might be more used by other plugins, and thus may need better support
+if (!window.cep) {
+  const fs = {
+    toString: () => "fs",
+    ERR_CANT_READ: 4,
+    ERR_CANT_WRITE: 6,
+    ERR_FILE_EXISTS: 10,
+    ERR_INVALID_PARAMS: 2,
+    ERR_NOT_DIRECTORY: 9,
+    ERR_NOT_FILE: 8,
+    ERR_NOT_FOUND: 3,
+    ERR_OUT_OF_SPACE: 7,
+    ERR_UNKNOWN: 1,
+    ERR_UNSUPPORTED_ENCODING: 5,
+    NO_ERROR: 0,
+    chmod: () => new NotImplemented("fs.chmod"),
+    deleteFile: () => new NotImplemented("fs.deleteFile"),
+    makedir: () => new NotImplemented("fs.makedir"),
+    readFile: () => new NotImplemented("fs.readFile"),
+    readdir: () => new NotImplemented("fs.readdir"),
+    rename: () => new NotImplemented("fs.rename"),
+    showOpenDialog: () => new NotImplemented("fs.showOpenDialog"),
+    showOpenDialogEx: () => new NotImplemented("fs.showOpenDialogEx"),
+    showSaveDialogEx: () => new NotImplemented("fs.showSaveDialogEx"),
+    stat: () => new NotImplemented("fs.stat"),
+    writeFile: () => new NotImplemented("fs.writeFile")
+  };
+
+  const process = {};
+
+  const util = {
+    DEPRECATED_API: 202,
+    ERR_INVALID_URL: 201,
+    openURLInDefaultBrowser: (url) => window.open(url, "blank"),
+    registerExtensionUnloadCallback: () => new NotImplemented("util.registerExtensionUnloadCallback"),
+    storeProxyCredentials: () => new NotImplemented("util.storeProxyCredentials"),
+  };
+  const encoding = {};
+
+  window.cep = { encoding, fs, process, util, };
 }
