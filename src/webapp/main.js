1426e1adf25c48b4b0cadf4612ee8a08cbbde34c
Rory Duncan <robust.rory@gmail.com>
2019-08-19 03:54:22 -0700

    Mount FuseFontsPlugin to webapp

--- src/webapp/main.js
+++ src/webapp/main.js
@@ -1,4 +1,9 @@
 import Photoshop from "Photoshop.html";
+import { loadData } from "stores/typefaces.js";
+import { loading } from "stores/app-settings.js";
 
 const target = document.getElementById("photoshop-emulation");
 new Photoshop({ target, });
+
+loadData();
+loading.set(false);
