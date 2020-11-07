2c3a776a867af9d92300a6becc557f5118a3a700
Rory Duncan <robust.rory@gmail.com>
2020-11-07 13:25:50 -0800

    Add Test running page

--- src/plugin/components/Tests.svelte
+++ src/plugin/components/Tests.svelte
@@ -1,14 +1,112 @@
 <script>
-    import { runAllTests } from "tests/tests.js";
+    import { allTests } from "tests/tests.js";
 
     function run() {
-        const results = runAllTests();
-        output.push(...results);
+        console.log(allTests);
+        allTests.forEach( test => {
+            results[test.test] = test.run();
+        });
     }
 
-    const output = [];
+    const results = {};
 
 </script>
 
+<style>
+
+    button {
+        background: var(--accent-color);
+    }
+
+    table {
+        margin: 1rem 0;
+        font-size: 1.2em;
+        width: 100%;
+        border: var(--panel-border);
+        border-collapse: collapse;
+    }
+
+    thead th {
+        padding: 0.5rem 0.5rem;
+        background: var(--panel-layer-2);
+        font-weight: 600;
+        text-align: left;
+    }
+
+    tr {
+        border-bottom: var(--panel-border);
+    }
+
+    td {
+        padding: 0.5rem 0.5rem;
+        border: var(--panel-border);
+    }
+
+    tr > td {
+        width: 33%
+    }
+
+    .running {
+        color: var(--accent-color);
+    }
+
+    .passed {
+        color: var(--success-color);
+    }
+
+    .failed {
+        color: var(--error-color);
+    }
+
+    .muted {
+        opacity: 0.5;
+    }
+
+    .empty {
+        text-align: center;
+        color: var(--muted);
+    }
+</style>
+
 <button on:click={run}>Run Tests</button>
 
+
+<table>
+    <thead>
+        <tr>
+            <th scope="test">Test</th>
+            <th scope="status">Status</th>
+            <th scope="result">Result</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each allTests as testRun}
+            <tr>
+                <td>{testRun.test}</td>
+                {#if results[testRun.test]}
+                    {#await results[testRun.test]}
+                        <td class:running={true}>Running...</td>
+                        <td>—</td>
+                    {:then result}
+                        <td class:passed={true}>Passed</td>
+                        <td>{result}</td>
+                    {:catch result}
+                        <td class:failed={true}>Failed</td>
+                        <td>{result}</td>
+                    {/await}
+                {:else}
+                    <td>
+                        Ready
+                    </td>
+                    <td class:muted={true}>—</td>
+                {/if}
+            </tr>
+        {:else}
+            <tr>
+                <td colspan="3" class="empty muted">
+                    <p>No tests to run</p>
+                </td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
--- src/plugin/tests/tests.js
+++ src/plugin/tests/tests.js
@@ -1,5 +1,21 @@
 
+import fontRepository from "repositories/fonts.js";
 
-export const runAllTests = () => {
-    return ["test1", "test2", "test3"];
-}
+export const testLoadFonts = () => ({
+    test: "Load Fonts from file system",
+    run: async () => fontRepository.load(false).then( fonts => `Loaded ${fonts.length} fonts`)
+});
+
+export const testLoadFontsCache = () => ({
+
+    test: "Load Fonts from local storage",
+    result: "Not Ran",
+    run: async () => fontRepository.load(true).then( fonts => `Loaded ${fonts.length} fonts from local storage`)
+});
+
+
+export const allTests = [
+    testLoadFonts(),
+    testLoadFontsCache(),
+];
+
